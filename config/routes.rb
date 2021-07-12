cffc87794f4367f286e5ec2c6d4b6e4970f62fe3
Neerav Patel <nickpatel.developer@gmail.com>
2021-07-12 11:30:52 -0400

    Change to better version API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :notes
+  namespace :api do
+    namespace :v1 do
+      resources :notes
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :users, only: [:create]
   post "/login", to: "users#login"
